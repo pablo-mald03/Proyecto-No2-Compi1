edb4328940527a7101376c7c5f1645eb6b3edc66
Pablo_Maldonado <pablomaldonadodev@gmail.com>
2026-04-16 01:23:25 -0600

    Se ha inicializado el repositorio

---
+++ web-proyecto-no2-compi1/src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+
+	/*librerias de bootstrap*/
+	import 'bootstrap/dist/css/bootstrap.min.css';
+	import 'bootstrap-icons/font/bootstrap-icons.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>YFERA</title>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="container-fluid p-0">
+	{@render children()}
+</div>
+
+<style>
+	:global(body, html) {
+		margin: 0;
+		padding: 0;
+		height: 100%;
+		overflow: hidden;
+	}
+</style>
