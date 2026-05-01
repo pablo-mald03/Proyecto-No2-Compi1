a9f9562e92b3c9bf26f5e16dac58ca1d4b766156
Pablo_Maldonado <pablomaldonadodev@gmail.com>
2026-05-01 01:01:08 -0600

    Se ha definido el manejo de ambitos para las funciones y se ha corregido la forma de comopilar

--- web-proyecto-no2-compi1/src/lib/backend/yferamodules/ModuloYFera.js
+++ web-proyecto-no2-compi1/src/lib/backend/yferamodules/ModuloYFera.js
@@ -5,6 +5,7 @@ import { TablaSimbolos } from "../semanticsyfera/TablaSimbolos";
 export class ModuloYFera {
     constructor(archivoRaiz, ast) {
         this.id = archivoRaiz.id;
+        this.parentId = archivoRaiz.parentId;
         this.nombre = archivoRaiz.name;
         this.ast = ast;
         
@@ -21,45 +22,4 @@ export class ModuloYFera {
         this.tablaSimbolos = new TablaSimbolos(); 
     }
 
-    async validarLoad(nodo, entornoActual) {
-        let rutaLoad = "";
-
-        if (nodo.uri.tipo === 'VALOR_CADENA') {
-            rutaLoad = nodo.uri.valor.trim();
-        } 
-
-        else if (nodo.uri.tipo === 'IDENTIFICADOR') {
-            const nombreVar = nodo.uri.valor;
-            const variable = entornoActual.getVariable(nombreVar);
-
-            if (!variable) {
-                this.compilador.agregarError(this.modulo.nombre, nombreVar, 'Semantico', `Variable '${nombreVar}' no definida.`, nodo.linea, nodo.columna);
-                return;
-            }
-
-            if (variable.tipoDato !== 'CADENA') {
-                this.compilador.agregarError(this.modulo.nombre, nombreVar, 'Semantico', `Instruccion load requiere una ruta de tipo 'String', se coloco tipo: ${variable.tipoDato}.`, nodo.linea, nodo.columna);
-                return;
-            }
-
-            if (variable.valor) {
-                rutaLoad = variable.valor.replace(/['"]/g, '').trim();
-            } else {
-                return; 
-            }
-        }
-
-        if (rutaLoad) {
-            const archivoSiguiente = await this.compilador.resolverPathRelativo(rutaLoad, this.modulo.id); 
-
-            if (archivoSiguiente) {
-                const moduloHijo = await this.compilador.procesarModuloY(archivoSiguiente);
-                if (moduloHijo) {
-                    this.modulo.modulosHijos.push(moduloHijo);
-                }
-            } else {
-                this.compilador.agregarError(this.modulo.nombre, rutaLoad, 'Semantico', `Error de load: No se encontro "${rutaLoad}".`, nodo.linea, nodo.columna);
-            }
-        }
-    }
 }
--- web-proyecto-no2-compi1/src/lib/backend/yferamodules/ValidadorSemanticoYfera.js
+++ web-proyecto-no2-compi1/src/lib/backend/yferamodules/ValidadorSemanticoYfera.js
@@ -19,7 +19,6 @@ export class ValidadorSemanticoYfera {
     }
 
     /*Recorrido por el AST para poder ir validando */
-
     async recorrerAST(nodos, entornoActual) {
         if (!nodos || !Array.isArray(nodos)) return;
 
@@ -28,23 +27,31 @@ export class ValidadorSemanticoYfera {
 
             switch (nodo.tipo) {
                 case 'DECLARACION_VARIABLE':
+                case 'INICIALIZACION_VARIABLE':
                     this.validarDeclaracion(nodo, entornoActual);
                     break;
 
-                case 'DEFINICION_FUNCION':
+                case 'FUNCION':
                     await this.validarFuncion(nodo, entornoActual);
                     break;
 
-                case 'FOR': 
-                    const entornoFor = new TablaSimbolos(entornoActual);
-                    await this.recorrerAST(nodo.instrucciones, entornoFor);
+                case 'FUNCION_MAIN': 
+                    const entornoMain = new TablaSimbolos(entornoActual);
+                   
+                    await this.recorrerAST(nodo.cuerpo, entornoMain);
                     break;
 
-                case 'LOAD_ARCHIVO':
+                case 'CICLO_WHILE': 
+                case 'CICLO_DO_WHILE':
+                    const entornoCiclo = new TablaSimbolos(entornoActual);
+                    await this.recorrerAST(nodo.cuerpo, entornoCiclo);
+                    break;
+
+                case 'LOAD_ARCHIVO': 
                     await this.validarLoad(nodo, entornoActual);
                     break;
 
-                // PENDIENTES OTROS CASOS
+                //Pendiente demas codigo
             }
         }
     }
@@ -57,12 +64,13 @@ export class ValidadorSemanticoYfera {
         }
 
         /*pendiente validacion de tipos */
-       
+
         const nuevoSimbolo = new Simbolo(nodo.id, nodo.tipoDato, nodo.valor, nodo.linea, nodo.columna, nodo.esArreglo);
 
         entorno.setVariable(nuevoSimbolo);
     }
 
+    /*Metodo que permite validar la semantica dentro de una funcion*/
     async validarFuncion(nodo, entornoActual) {
 
         const entornoLocal = new TablaSimbolos(entornoActual);
@@ -75,6 +83,48 @@ export class ValidadorSemanticoYfera {
         }
 
         await this.recorrerAST(nodo.cuerpo, entornoLocal);
+    }
+
+    /*Metodo que permite validar el load recursivamente en cada .y llamado*/
+    async validarLoad(nodo, entornoActual) {
+        let rutaLoad = "";
+
+        if (nodo.uri.tipo === 'VALOR_CADENA') {
+            rutaLoad = nodo.uri.valor.trim();
+        }
+        else if (nodo.uri.tipo === 'IDENTIFICADOR') {
+            const nombreVar = nodo.uri.valor;
+            const variable = entornoActual.getVariable(nombreVar);
+
+            if (!variable) {
+                this.compilador.agregarError(this.modulo.nombre, nombreVar, 'Semantico', `Variable '${nombreVar}' no definida.`, nodo.linea, nodo.columna);
+                return;
+            }
+            if (variable.tipoDato !== 'CADENA') {
+                this.compilador.agregarError(this.modulo.nombre, nombreVar, 'Semantico', `El LOAD requiere una ruta STRING, se recibió ${variable.tipoDato}.`, nodo.linea, nodo.columna);
+                return;
+            }
+
+            if (variable.valor) {
+
+                rutaLoad = variable.valor.trim();
+            } else {
+                return;
+            }
+        }
+
+        if (rutaLoad) {
+            const archivoSiguiente = await this.compilador.resolverPathRelativo(rutaLoad, this.modulo.parentId);
+
+            if (archivoSiguiente) {
+                const moduloHijo = await this.compilador.procesarModuloY(archivoSiguiente);
+                if (moduloHijo) {
+                    this.modulo.modulosHijos.push(moduloHijo);
+                }
+            } else {
+                this.compilador.agregarError(this.modulo.nombre, rutaLoad, 'Semantico', `Error de Load: No se encontró el archivo '${rutaLoad}'.`, nodo.linea, nodo.columna);
+            }
+        }
     }
 
 }
